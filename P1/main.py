2a6a42eafee27b223024eb611c3143b3feb41096
jetwardenier <jet.wardenier@gmail.com>
2021-02-08 22:07:43 +0100

    gezeik met dinges

---
+++ P1/main.py
@@ -0,0 +1,8 @@
+from perceptron import Perceptron
+
+def test():
+    p = Perceptron([0.1, 0.5, 0.1],2)
+    x = p.activate([0.0,0.2,0.1])
+    print(x)
+test()
+
---
+++ P1/perceptron.py
@@ -0,0 +1,20 @@
+# import random
+class Perceptron():
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def activate(self, __input:list): 
+        for i in range(0, len(self.weights)):
+            x = 0
+            x += self.weights[i] * __input[i] + self.bias
+        if x >= 0:
+            return 1
+        else:
+            return 0
+
+    def __str__(self): 
+        return 'weights' + {self.weights} + 'bias' + {self.bias}
+
+class PerceptronLayer():
+    def __init__(self, list)
