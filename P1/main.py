d19370a53b5567c612996bb8ce339ccb5199ff89
jetwardenier <jet.wardenier@gmail.com>
2021-02-17 12:01:53 +0100

    perceptron unit + tests

--- P1/main.py
+++ P1/main.py
@@ -1,5 +1,5 @@
 from perceptron import Perceptron
-# Perceptron test met weigths 0.1, 0.5 en 0.1 en bias 2 
+# Perceptron test met weigths 0.1, 0.5 en 0.1 en bias 2
 
 def test():
     p = Perceptron([0.1, 0.5, 0.1],2)
@@ -8,42 +8,65 @@ def test():
 #test()
 
 ## Invert(not) gate zet input 1 om in 0 en vice versa
-def invert(): 
+def invert():
     p = Perceptron([-1], 0.5)
-    output = p.activate([0])
-    print(output)
-#invert()
+    i1 = [0]
+    i2 = [1]
+    inv1 = p.activate(i1)
+    inv2 = p.activate(i2)
+    print(i1, i2, inv1, inv2)
+invert()
 
 ## AND gate
 def andgate():
-    p = Perceptron([1, 1],1)
-    output = p.activate([1,0])
-    print(output)
+    p = Perceptron([0.5, 0.5],-1)
+    and1 = p.activate([1,1])
+    and2 = p.activate([1,0])
+    and3 = p.activate([0,1])
+    and4 = p.activate([0,0])
+    print(and1, and2, and3, and4)
 andgate()
 
 ## OR gate (inclusive)
 def orgate():
-    p = Perceptron([0.5, 0.5],-0.25)
-    output = p.activate([1,0])
-    print(output)
-#orgate()
+    p = Perceptron([1, 1],-1)
+    or1 = p.activate([1, 1])
+    or2 = p.activate([1, 0])
+    or3 = p.activate([0, 1])
+    or4 = p.activate([0, 0])
+    print(or1, or2, or3, or4)
+orgate()
 
 ## NOR gate (overal 0 behalve bij x1 = x2 = x3 =0)
 def norgate():
     p = Perceptron([-0.5,-0.5,-0.5], 0.25)
-    output = p.activate([0,0,0])
-    print(output)
-#norgate()
+    nor1 = p.activate([1, 1, 1])
+    nor2 = p.activate([1, 1, 0])
+    nor3 = p.activate([1, 0, 1])
+    nor4 = p.activate([1, 0, 0])
+    nor5 = p.activate([0, 1, 1])
+    nor6 = p.activate([0, 1, 0])
+    nor7 = p.activate([0, 0, 1])
+    nor8 = p.activate([0, 0, 0])
+    print(nor1, nor2, nor3, nor4, nor5, nor6, nor7, nor8)
+norgate()
 
 ## Party poort 2.8
 def partypoort():
-    p = Perceptron([0.6, 0.3, 0.2], 0.4)
-    output = p.activate([0,0,0])
-    print(output)
-#partypoort()
+    p = Perceptron([0.6, 0.3, 0.2], -0.4)
+    party1 = p.activate([1, 1, 1])
+    party2 = p.activate([1, 1, 0])
+    party3 = p.activate([1, 0, 1])
+    party4 = p.activate([1, 0, 0])
+    party5 = p.activate([0, 1, 1])
+    party6 = p.activate([0, 1, 0])
+    party7 = p.activate([0, 0, 1])
+    party8 = p.activate([0, 0, 0])
+    print(party1, party2, party3, party4, party5, party6, party7, party8)
+partypoort()
 
-def testLayer(): 
-    l = (5, [0.5, -0.5], 2)
-    l = l.activate_layer([1,1,1])
-    print(l)
-testLayer()
+# def testLayer():
+#     l = (5, [0.5, -0.5], 2)
+#     l = l.activate_layer([1,1,1])
+#     print(l)
+# testLayer()
--- P1/perceptron.py
+++ P1/perceptron.py
@@ -3,53 +3,52 @@ class Perceptron():
     def __init__(self, weights, bias):
         self.weights = weights
         self.bias = bias
-    
 
-## STEP-activatie functie returnt 1 bij input groter dan 0 en anders 0 
-    def activate(self, input:list): 
+    ## STEP-activatie functie returnt 1 bij input groter dan 0 en anders 0
+    def activate(self, _input: list):
+      #  if len(self.weights) != len(input):
+      #      return None
+
+        output = 0
         for i in range(0, len(self.weights)):
-            output = 0
-            output = (self.weights[i] * self.input[i])
-            output = output + self.bias
-        if output <= 0:
-            return 0
-        else:
-            return 1
+            output += (self.weights[i] * _input[i])
+        return 1 if output + self.bias >= 0 else 0
 
-            
-# ## String functie
-#     def __str__(self): 
+
+        # ## String functie
+#     def __str__(self):
 #         return 'weights:' {self.weights} 'bias:' {self.bias}
 
 # __str__()
 
-class PerceptronLayer():
-    def __init__(self, n, weights, bias):
-        self.layer = []
-        for i in n: 
-            self.layer.append(Perceptron())
+# class PerceptronLayer():
+#     def __init__(self, n, weights, bias):
+#         self.layer = []
+#         for i in n:
+#             self.layer.append(Perceptron())
+#
+#     def activate_layer()
+#         for i in range(0, len(Perceptron.weights)):
+#             outputlayer = 0
+#             outputlayer = (Perceptron.weights[i] * Perceptron.input[i])
+#             outputlayer = outputlayer + perceptron.bias
+#         if outputlayer <= 0:
+#             return 0
+#         else:
+#             return 1
 
-    def activate_layer()
-        for i in range(0, len(Perceptron.weights)):
-            outputlayer = 0
-            outputlayer = (Perceptron.weights[i] * Perceptron.input[i])
-            outputlayer = outputlayer + perceptron.bias
-        if outputlayer <= 0: 
-            return 0
-        else: 
-            return 1
 
-class PerceptronNetwork(): 
-    def __init__(self, n, weights, bias)
-        self.network = []
-        for i in n: 
-            self.network.append(PerceptronLayer())
-
-    def feedforward()
-        for i in range(0, len(PerceptronLayer.weights)):
-            outputnetwork = 0
-            outputnetwork = (PerceptronLayer.weights[i] * Perceptron.input[i])
-            outputnetwork = outputnetwork + self.bias
-        if outputnetwork <= 0: 
-            return 0
-        else return 1
+# class PerceptronNetwork():
+#     def __init__(self, n, weights, bias)
+#         self.network = []
+#         for i in n:
+#             self.network.append(PerceptronLayer())
+#
+#     def feedforward()
+#         for i in range(0, len(PerceptronLayer.weights)):
+#             outputnetwork = 0
+#             outputnetwork = (PerceptronLayer.weights[i] * Perceptron.input[i])
+#             outputnetwork = outputnetwork + self.bias
+#         if outputnetwork <= 0:
+#             return 0
+#         else return 1
